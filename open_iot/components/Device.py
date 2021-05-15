c7dd62060be78fb058ea1d08876ae30c9b675be4
Mostafa <mostafa_gho@yahoo.com>
2021-05-15 16:32:58 +0430

    some minor changes

--- open_iot/components/Device.py
+++ open_iot/components/Device.py
@@ -8,7 +8,9 @@ import time
 
 class Device():
     
-    def __init__(self, ID, hostname, name='ganjine', is_server=False):
+    def __init__(self, ID, hostname, name='ganjine', is_server=False, verbose=False):
+        self.verbose = verbose
+        self.is_server = is_server
         self.name = name
         self.ID = ID
         self._hostname = hostname
@@ -21,7 +23,8 @@ class Device():
     def create_mqtt_client(self):
         self.client = mqtt.Client(self.name + "_" + self.ID)
 
-        self.client.on_log = self._on_log
+        if self.verbose:
+            self.client.on_log = self._on_log
         self.client.on_connect = self._on_connect
 
         self.client.connect(self._hostname)
@@ -32,7 +35,7 @@ class Device():
         else:
             self.topics[topic_name] = Topic(
                 self, 
-                '{0}/{1}/{2}/{3}'.format('from_server', self.name, self.ID, category), 
+                '{0}/{1}/{2}/{3}'.format('to_server' if self.is_server else 'from_server', self.name, self.ID, category), 
                 callerback,
                 topic_name,
                 qos=qos
@@ -43,8 +46,6 @@ class Device():
         pass
 
     def _on_connect(self, client, userdata, flags, rc):
-        self.request_settings()
-
         if not self.is_init:
             for topic in self.topics:
                 self.topics[topic].do_subscribe()
@@ -54,23 +55,9 @@ class Device():
     def _on_log(self, client, userdata, level, buf):
         print("log: ", buf)
 
-    def request_settings(self):
-        pass
-        # fetch settings from server
-        self.add_topic(
-            'settings',
-            self.set_settings,
-            'settings',
-        )
-
-        self.publisher('settings')
-
-    def set_settings(self, client, userdata, message):
-        self.settings = json.loads(message.payload)
-
     def publisher(self, topic, payload='', qos=0):
         self.client.publish(
-            '{0}/{1}/{2}/{3}'.format('to_server', self.name, self.ID, topic),
+            '{0}/{1}/{2}/{3}'.format('from_server' if self.is_server else 'to_server', self.name, self.ID, topic),
             payload=payload, 
             qos=qos
             )
